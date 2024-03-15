fe63dd7678ae737fb72fa486a1a99bdcec4cdc4e
Julie Poulain <juliempoulain@gmail.com>
2024-03-15 10:58:42 -0700

    inserted fetch and .thn functions within fetchBooks

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    renderBooks(data);
+  })
 }
 
 function renderBooks(books) {
